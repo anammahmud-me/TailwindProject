fa57fe34deda59b857f0293ece2336dd97970fa0
Anam Mahmud <anammahmud131@gmail.com>
2024-05-26 15:23:36 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      "sm": "450px",
+      "md": "740px",
+      "lg": "1050px",
+    },
+    fontFamily: {
+      "nunito": "'Nunito', 'sans-serif'"
+    },
+    colors: {
+      "hero": "#37517e",
+      "white": "#ffffff",
+      "secBg": "#f5f6f8",
+      "sky": "#46afe0",
+      "muted": "#c3cbd9",
+      "header": "#37517e",
+      "btnColor": "#47b2e4",
+      "check": "#0c9957",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
